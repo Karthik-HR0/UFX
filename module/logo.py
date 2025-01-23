9c0c1a2ad7f07350caf822cf03be66eebf0a8340
Fussion-xp <karthikhr1623@gmail.com>
2025-01-23 16:32:53 +0530

    Add files via upload

---
+++ module/logo.py
@@ -0,0 +1,20 @@
+class Logo:
+    @staticmethod
+    def display():
+        """Display the logo/banner of the tool."""
+        banner = r"""
+
+
+ __ __  _____  __ __     
+|  T  T|     ||  T  T    
+|  |  ||   __j|  |  |    
+|  |  ||  l_  l_   _j    
+|  :  ||   _] |     |    
+l     ||  T   |  |  |    
+ \__,_jl__j   |__j__|    
+                         @Karthik-HR0        
+
+    URLS FOR EXPLOIT  (UFX)
+                       
+        """
+        print(banner)
